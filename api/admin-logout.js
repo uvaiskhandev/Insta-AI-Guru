a5117db6d2037953d606517949c612d4837da8c2
Uvais Khan <uvaismoosewala@gmail.com>
2026-04-15 10:45:36 +0530

    Delete api/admin-logout.js

--- api/admin-logout.js
+++
@@ -1,8 +0,0 @@
-module.exports = async (req, res) => {
-  res.setHeader(
-    "Set-Cookie",
-    "admin_session=; HttpOnly; Path=/; Max-Age=0; SameSite=Strict; Secure"
-  );
-
-  return res.status(200).json({ success: true });
-};
